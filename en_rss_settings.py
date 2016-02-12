f6a2392439d24eddda20636dd7330fc3667d7469
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-12 10:21:54 +0000

    adding more logging

--- en_rss_settings.py
+++ en_rss_settings.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 api = Api(app)
 
 # App Settings
+app.config['BUNDLE_ERRORS'] = True
 SERVICES = {
     'eve-news': {'name': 'EVE Online News', 'official': True},
     'eve-blogs': {'name': 'EVE Online Dev Blogs', 'official': True},
@@ -64,17 +65,23 @@ class ExternalCharacterSettings(Resource):
         parser = reqparse.RequestParser()
         
         for feed in SERVICES:
-            parser.add_argument(feed, type=bool, help=SERVICES[feed]['name'])
+            parser.add_argument(
+                feed,
+                type=bool,
+                required=True
+            )
 
         args = parser.parse_args(strict=True)
-
-        app.logger.info('Put request, args: {}'.format(args))
         
         client = get_client()
         character_settings = client.get(client.key(SETTINGS_KIND, character_id))
         
         if character_settings is None:
+            app.logger.info('Adding new stored settings for {} with the following values: {}'.format(character_id, args))
             character_settings = datastore.Entity(client.key(SETTINGS_KIND, character_id))
+        
+        else:
+            app.logger.info('Updating the stored settings for {} with the following values: {}'.format(character_id, args))
         
         for feed in SERVICES:
             if feed in args:
@@ -82,23 +89,19 @@ class ExternalCharacterSettings(Resource):
 
         client.put(character_settings)
 
-        app.logger.info('character_settings: {}'.format(character_settings))
-
-        return dict(character_settings), 204
+        return {}, 204
 
 
 class InternalSettings(Resource):
     def get(self, feed_id):
         client = get_client()
         
+        app.logger.info('Internal request for feed {}.'.format(feed_id))
+        
         query = client.query(kind=SETTINGS_KIND)
         query.add_filter(feed_id, '=', 'TRUE')
         
-        data = [x.id for x in query.fetch()]
-        
-        app.logger.info('{}'.format(data))
-        
-        return data
+        return [x.id for x in query.fetch()]
     
 
 api.add_resource(ExternalSettings, '/external/')
